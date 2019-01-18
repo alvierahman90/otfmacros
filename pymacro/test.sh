75587f7b3a9d36ea522bd07685d4c187cb83294d
Alvie Rahman <alvierahman90@gmail.com>
2019-01-18 22:37:48 +0000

    Add ability to source from external macro files

--- pymacro/test.sh
+++ pymacro/test.sh
@@ -1,4 +1,4 @@
 #/usr/bin/env sh
-./pymacro -sm tests/test_macros_plural -m tests/test_macros_biology tests/test_input tests/test_actual_output
-diff tests/test_actual_output tests/test_expected_output
+./pymacro -s tests/test_input tests/test_actual_output
+git diff tests/test_actual_output tests/test_expected_output
 
