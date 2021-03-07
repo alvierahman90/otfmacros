5648638fcb6edfcc38590da3baf7ba5c488d9a44
Alvie Rahman <alvierahman90@gmail.com>
2021-03-07 14:33:24 +0000

    update readme, move python implementation to another repo, merge syntax.md with readme.md, remove test.sh

--- pymacro/test.sh
+++
@@ -1,3 +0,0 @@
-#/usr/bin/env sh
-cat tests/test_input | ./pymacro > tests/test_actual_output
-git diff tests/test_actual_output tests/test_expected_output
--- test.sh
+++
@@ -1,4 +0,0 @@
-#/usr/bin/env sh
-./pymacro -sm tests/test_macros_plural -m tests/test_macros_biology tests/test_input tests/test_actual_output
-diff tests/test_actual_output tests/test_expected_output
-
